eec0ef6e502e1ac034eafcd3dbc9ed661bb8d4a0
Raion <sweet.raion@gmail.com>
2020-03-30 10:59:05 +0900

    Fix the errors.

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,15 +63,7 @@ function startAnimation() {
 
 /////////////////////////////////////////////
 
-var timeleft = 9;
-var downloadTimer = setInterval(function () {
-    document.getElementById("countdown").innerHTML = timeleft;
-    timeleft -= 1;
-    if (timeleft < 0) {
-        clearInterval(downloadTimer);
-        document.getElementById("countdown").innerHTML = "0"
-    }
-}, 1000);
+
 
 /////////////////////////////////////////////
 
